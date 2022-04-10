16a6c334a2a8775d7594759e16c0c781ff2c6ecf
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-10 16:22:52 +0300

    Add EXM007_Magic

---
+++ EXM007_Magic/Program.cs
@@ -0,0 +1,42 @@
+﻿int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.Clear();
+Console.SetCursorPosition(xa, ya);
+Console.Write("@");
+Console.SetCursorPosition(xb, yb);
+Console.Write("@");
+Console.SetCursorPosition(xc, yc);
+Console.Write("@");
+
+int x = xa, y = ya;
+
+int count = 100000;
+
+while (count > 0)
+{
+    int what = new Random().Next(0, 3);
+
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("@");
+    count--;
+}
