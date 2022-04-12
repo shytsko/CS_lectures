ec0ec232f6e4aebf6aa241cbede806da8419a2ee
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-12 07:26:59 +0300

    Add examples from seminar 1

---
+++ SEM001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+using System;
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqr = number * number;
+
+Console.WriteLine(sqr);
---
+++ SEM002/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая на вход принимает одно число
+// (N), а на выходе показывает все целые числа в промежутке от -N
+// до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int start;
+if (N > 0)
+{
+    start = 0 - N;
+}
+else
+{
+    start = N;
+}
+int end;
+if (N > 0)
+{
+    end = N;
+}
+else
+{
+    end = 0 - N;
+}
+int i = start;
+while (i <= end)
+{
+    Console.Write(i + ", ");
+    i++;
+}
