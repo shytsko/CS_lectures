25f6ef3c2be8f9181b62408fb87ed3fcc993e30e
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-21 21:53:44 +0300

    Add sem003, sem004

---
+++ SEM003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int countDigit = 0;
+
+// while(number != 0)
+// {
+//     number /= 10;
+//     countDigit++;
+// }
+
+for (int numberTemp = number; numberTemp != 0; numberTemp /= 10, countDigit++);
+
+Console.WriteLine($"Количество цифр в числе {number}: {countDigit}");
---
+++ SEM004/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке
+// [1,0,1,1,0,1,0,0]
+
+int[] array = GenerateArray(8);
+PrintArray(array);
+
+int[] GenerateArray(int len)
+{
+    int[] array = new int[len];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }   
+}
