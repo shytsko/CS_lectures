1890b23d6424e8e84950eb5c917fa378ec8bfaec
Vladimir Shytsko <shytsko.v@gmail.com>
2022-05-20 13:27:42 +0300

    Seminar 9

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int n = 1111111111;
+
+int sum = SumDigit(n);
+Console.WriteLine(sum);
+
+int SumDigit(int n)
+{
+    if (n < 10)
+        return n;
+    else
+        return n % 10 + SumDigit(n / 10);
+}
